8d3a0e8ef6a7226540f8e68fbf327087623a5d0d
Sören L <daftcap@web.de>
2017-06-29 13:31:48 +0200

    Add files via upload
    GET Requests und package.json hinzugefügt

--- Codetesting/mainApp.js
+++ Codetesting/mainApp.js
@@ -1,97 +1,60 @@
-// Bind Modules
-var express = require("express");
-var bodyParser = require('body-parser');
+// Modules
+const express = require('express');
+const bodyParser = require('body-parser');
 
-// Bind variable to Object for access
-var app = express();
-var jsonParser = bodyParser.json();
+// Access Variables
+const app = express();
+const jsonParser = bodyParser.json();
 
-// Constant variables for Settings
+// Settings
 const settings = {
 	port: 3000
-	// additional settings
 };
 
 //Testing instead of using Google's API
 var citytesting = [
 	{id: 1, city: "Gummersbach", 	rent: 	450, renttype: "warm", size: 9,  roomqty: 1},
 	{id: 2, city: "Köln", 			rent: 	400, renttype: "warm", size: 49, roomqty: 2},
-	{id: 3, city: "Grevenbroich", 	rent:	300, renttype: "kalt", size: 75, roomqty: 4}
+	{id: 3, city: "Grevenbroich", 	rent:	300, renttype: "kalt", size: 75, roomqty: 4},
+	{id: 4, city: "Köln", 			rent: 	500, renttype: "warm", size: 53, roomqty: 2}
 ]
 
-//Errorhandling
-app.use(function(err,req,res,next)){}
-
-
-//Testfunction
+// GET REQUESTS
+// Test Request
 app.get('/', function(req, res){
 	res.send('Hello World!');
 });
 
+// Show all the available offers
+app.get('/offers/all', function(req,res){
 
-//Log mit Pfad und Zeitangabe für jeden Request-Pfad
-app.use(function(req,res,next)){}
-
-//Routing
-
-//Bind Path to Constante
-const user = require('./user');
-//Mounting Router for "user" in mainApp
-app.use("/user".user)
-
-//Methodes
-
-// GET Request on Path "/"
-app.get("/", function(req,res)){
 	res.status(200).json(citytesting);	
 });
-// POST Request on Path "/"
-app.post('/', jsonParser, function(req, res){
-	citytesting.push(req.body);
-	res.type('plain').send('Added!');
-});
 
-
-// GET Request on Path "/offers"
-app.get("/offers", function(req,res)){
-	var offers = req.params.offer;
+// Show specific ID
+app.get('/offers/:id', function (req, res) {
+    
+	var id = req.params.id;
 	
 	var filteredData = citytesting.filter(function(value, index, arr){
-		return value.offers == offers;
+		return value.id == id;
 	});
 	
 	if(filteredData.length >= 1){
 		res.status(200).json(filteredData);	
 	}
 	else{
-		res.status(404).end();	
+		 var message = {
+                "error": {
+                    "message": "Anzeige mit der ID " + req.params.id + " nicht gefunden!"
+                }
+            }
+            res.status(404).json(message);	
 	}
 });
-// POST Request on Path "/offers"
-app.post("/offers", function(req,res))
-
-
-// GET Request on Path "/offers/:id"
-app.get("/offers", function(req,res)){
-	var offers = req.params.offers;
-	
-	var filteredData = citytesting.filter(function(value, index, arr){
-		return value.offers == offers;
-	});
-	
-	if(filteredData.length >= 1){
-		res.status(200).json(filteredData);	
-	}
-	else{
-		res.status(404).end();	
-	}
-});
-// DEL Request on Path "/offers/:id"
-app.del("/offers/:id", function(req,res){
-	res.send("DEL /offers/:id")
-});
-// GET Request on Path "/offers/:city"
-app.get("/offers/:city", function(req,res)){
+// Filter by cities
+app.get('/offers/city/:city', function (req, res) {
+    
 	var city = req.params.city;
 	
 	var filteredData = citytesting.filter(function(value, index, arr){
@@ -102,72 +65,150 @@ app.get("/offers/:city", function(req,res)){
 		res.status(200).json(filteredData);	
 	}
 	else{
-		res.status(404).end();	
+		 var message = {
+                "error": {
+                    "message": "Keine Anzeigen in " + req.params.city + " gefunden!"
+                }
+            }
+            res.status(404).json(message);	
 	}
 });
 
+// Filter the City by Room Size
+app.get('/offers/city/:city/size/:size', function (req, res) {
+    
+	var city = req.params.city;
+	
+	var filteredData = citytesting.filter(function(value, index, arr){
+		return value.city == city;
+	});
+	
+	if(filteredData.length >= 1){
+			var size = req.params.size;
 
-// GET Request on Path "/offers/:city/:size"
-app.get("/offers/:city/:size", function(req,res)){
-	var size = req.params.size;
+			var aFilteredData = citytesting.filter(function(value, index, arr){
+				if( value.city == city && value.size <= size){
+					return value.city == city;
+					return value.size == size;
+				}
+			});
+	
+			if(aFilteredData.length >= 1){
+				res.status(200).json(aFilteredData);	
+			}
+			else{
+				 var message = {
+		                "error": {
+		                    "message": "Keine Anzeigen in " + req.params.city + " mit der Maximalgröße von " + req.params.size + " Quadratmeter gefunden!"
+		                }
+		            }
+		            res.status(404).json(message);	
+			}
+	}
+	else{
+		 var message = {
+                "error": {
+                    "message": "Keine Anzeigen in " + req.params.city + " gefunden!"
+                }
+            }
+            res.status(404).json(message);	
+	}
+});
+// Filter the City by Room Quantity
+app.get('/offers/city/:city/roomqty/:roomqty', function (req, res) {
+    
+	var city = req.params.city;
 	
 	var filteredData = citytesting.filter(function(value, index, arr){
-		return value.size == size;
+		return value.city == city;
 	});
 	
 	if(filteredData.length >= 1){
-		res.status(200).json(filteredData);	
+			var roomqty = req.params.roomqty;
+
+			var aFilteredData = citytesting.filter(function(value, index, arr){
+				if( value.city == city && value.roomqty == roomqty){
+					return value.city == city;
+					return value.roomqty == roomqty;
+				}
+			});
+	
+			if(aFilteredData.length >= 1){
+				res.status(200).json(aFilteredData);	
+			}
+			else{
+				 var message = {
+		                "error": {
+		                    "message": "Keine Anzeigen in " + req.params.city + " mit der Raumanzahl von " + req.params.roomqty + " gefunden!"
+		                }
+		            }
+		            res.status(404).json(message);	
+			}
 	}
 	else{
-		res.status(404).end();	
+		 var message = {
+                "error": {
+                    "message": "Keine Anzeigen in " + req.params.city + " gefunden!"
+                }
+            }
+            res.status(404).json(message);	
 	}
 });
-// GET Request on Path "/offers/:city/:rent"
-app.get("/offers/:city/:rent", function(req,res)){
-	var rent = req.params.rent;
+//Filter the City by Rent Type
+app.get('/offers/city/:city/renttype/:renttype', function (req, res) {
+    
+	var city = req.params.city;
 	
 	var filteredData = citytesting.filter(function(value, index, arr){
-		return value.rent == rent;
+		return value.city == city;
 	});
 	
 	if(filteredData.length >= 1){
-		res.status(200).json(filteredData);	
+			var renttype = req.params.renttype;
+
+			var aFilteredData = citytesting.filter(function(value, index, arr){
+				if( value.city == city && value.renttype == renttype){
+					return value.city == city;
+					return value.renttype == renttype;
+				}
+			});
+	
+			if(aFilteredData.length >= 1){
+				res.status(200).json(aFilteredData);	
+			}
+			else{
+				 var message = {
+		                "error": {
+		                    "message": "Keine Anzeigen in " + req.params.city + " mit der Mietart " + req.params.renttype + " gefunden!"
+		                }
+		            }
+		            res.status(404).json(message);	
+			}
 	}
 	else{
-		res.status(404).end();	
+		 var message = {
+                "error": {
+                    "message": "Keine Anzeigen in " + req.params.city + " gefunden!"
+                }
+            }
+            res.status(404).json(message);	
 	}
 });
-// GET Request on Path "/offers/:city/:renttype"
-app.get("/offers/:city/:renttype", function(req,res)){
-	var renttype = req.params.offer;
-	
-	var filteredData = citytesting.filter(function(value, index, arr){
-		return value.renttype == renttype;
-	});
-	
-	if(filteredData.length >= 1){
-		res.status(200).json(filteredData);	
-	}
-	else{
-		res.status(404).end();	
-	}
-});
-// GET Request on Path "/offers/:city/:roomqty"
-app.get("/offers/:city/:roomqty", function(req,res)){
-	var roomqty = req.params.roomqty;
-	
-	var filteredData = citytesting.filter(function(value, index, arr){
-		return value.roomqty == roomqty;
-	});
-	
-	if(filteredData.length >= 1){
-		res.status(200).json(filteredData);	
-	}
-	else{
-		res.status(404).end();	
-	}
-});
+//Filter the City and Rent Type by Rent Amount
+//app.get('/offers/city/:city/renttype/:renttype/rent/:rent', function (req, res) {
+
+//Add another ad
+//app.post('/offers', jsonParser, function(req, res){
+//	citytesting.push(req.body);
+//	res.type('plain').send('Added!');
+//});
+
+
+//Delete an existing ad
+//app.delete("/offers/:id", function(req,res){
+//	res.send("DEL /offers/:id")
+//});
 
 app.listen(settings.port, function(){
-	console.log("Service is running on port"+settings.port+".");
+	console.log("Service is running on port "+settings.port+".");
 });
