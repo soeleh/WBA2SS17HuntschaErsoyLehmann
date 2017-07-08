a8fde94bca715a28ee33aa3a949d7c9b972d5334
emreersoyy <emre.ersoy1995@gmail.com>
2017-07-08 19:36:22 +0200

    dienstnutzer hinzugefügt

---
+++ dienstnutzer/package.js
@@ -0,0 +1,28 @@
+{
+  "name": "dienstnutzer",
+  "version": "1.0.0",
+  "description": "nutzt dienst",
+  "main": "dienstnutzer.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "node dienstnutzer.js"
+  },
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "async": "^2.5.0",
+    "body-parser": "^1.17.2",
+    "express": "^4.15.3",
+    "jsonschema": "^1.1.1",
+    "redis": "^2.7.1"
+  },
+  "devDependencies": {},
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/soeleh/huntschaersoylehmann.git"
+  },
+  "bugs": {
+    "url": "https://github.com/soeleh/huntschaersoylehmann/issues"
+  },
+  "homepage": "https://github.com/soeleh/huntschaersoylehmann#readme"
+}
