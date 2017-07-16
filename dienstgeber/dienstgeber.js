32d137c3851dea5b1c606e6737017e7de3a0bd89
Sören L <daftcap@web.de>
2017-07-16 17:30:57 +0200

    Coded for local data storage

--- dienstgeber/dienstgeber.js
+++ dienstgeber/dienstgeber.js
@@ -1,5 +1,5 @@
-<<<<<<< Updated upstream:dienstgeber/dienstgeber.js
 // Modules
+const express = require('express'),
 		bodyParser = require('body-parser'),
 		fs = require('fs'),
 		async = require('async'),
@@ -7,16 +7,17 @@
 		Validator = require('jsonschema').Validator;
 
 // Access variables
-var app = express(),
-		jsonParser = bodyParser.json(),
-		v = new Validator();
-	//db = redis.createClient();
-		app.use(jsonParser);
+global.app = express(),
+global.jsonParser = bodyParser.json(),
+global.v = new Validator();
+		global.offers = require ("./data/testdata.json");
+		//db = redis.createClient();
+app.use(jsonParser);
 
 // Settings for given Port or Port 3000
 const settings = {
 	port: process.env.PORT || 3000,
-
+	datafile: "./data/testdata.json"
 };
 
 // JSON scheme
@@ -54,81 +55,12 @@ v.addSchema(offerScheme, '/oneOffer');
 var data_offers = require('./data/offers.js');
 data_offers.init(app);
 
-//Testdata
+/*Testdata
 var data_testdata = require('./data/testdata.js');
-data_testdata.init(app);
+data_testdata.init(app);*/
 
 
 //Starting the app
 app.listen(settings.port, function(){
 	console.log("Service is running on port "+settings.port+".");
 });
-=======
-// Modules
-global.express = require('express');
-global.bodyParser = require('body-parser');
-global.fs = require('fs');
-global.async = require('async');
-global.redis = require('redis');
-global.Validator = require('jsonschema').Validator;
-
-// Access variables
-global.app = express();
-global.jsonParser = bodyParser.json();
-global.v = new Validator();
-
-//GIBT ERROR BEI HEROKU (application error H10)
-global.db = redis.createClient();
-
-app.use(jsonParser);
-
-// Settings
-const settings = {
-	port: process.env.PORT || 3000,
-};
-
-// JSON scheme
-var offerScheme = {
-    "id": "/oneOffer",
-    "type": "object",
-    "properties": {
-        "id": { "type": "number" },
-        "city": { "type": "string" },  
-        "rent": {"type": "number"},
-        "renttype": { "type": "string" },
-        "size": {"type": "number"},
-        "roomqty": {"type": "number"}
-    },
-    "required": ["id", "city", "rent", "renttype", "size", "roomqty"],
-    "additionalProperties": false
-};
-
-v.addSchema(offerScheme, '/oneOffer');
-
-/*-------Offer example-------
-{
-	"id": 			1, 
-	"city": 		"Gummersbach", 	
-	"rent": 		450, 
-	"renttype": 	"warm", 
-	"size": 		9,  
-	"roomqty": 		1
-}
-*/
-
-//Add data
-
-//Offers
-var data_offers = require('./data/offers.js');
-data_offers.init(app);
-
-//Testdata
-var data_testdata = require('./data/testdata.js');
-data_testdata.init(app);
-
-
-//Starting the app
-app.listen(settings.port, function(){
-	console.log("Service is running on port "+settings.port+".");
-});
->>>>>>> Stashed changes:dienstgeber.js
